c70ecfc0e05f2703f18c33fa47fb6263f0600d7a
Mike Ford <mike@mikefordweb.com>
2015-08-27 20:07:10 -0500

    port update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 //var port     = process.env.PORT || 3000;
-var port     = 3000;
+var port     = 80;
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
